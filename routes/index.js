e3feb3eceb69e24164ee4f261297bb16ebbd2f2e
mowaiswarsi <muhammadowaiswarsi@gmail.com>
2020-07-28 16:02:41 +0500

    hello

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use('/users', require('./users.js'))
-router.use('/posts', require('./posts.js'))
-router.use('/company',require('./company'))
-
-module.exports = router;
